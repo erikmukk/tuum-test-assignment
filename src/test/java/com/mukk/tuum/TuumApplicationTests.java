415ac4797356a0f35a741e1d67ae1fb85ae0809c
Erik Mukk <mukkerik@gmail.com>
2022-10-04 15:45:11 +0300

    Initial commit

---
+++ src/test/java/com/mukk/tuum/TuumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukk.tuum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
